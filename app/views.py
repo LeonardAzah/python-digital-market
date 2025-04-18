550ae962d1cd8b81806fe505c61aae7cbbe2b2bf
LeonardAzah <isongleonard@gmail.com>
2025-04-18 21:29:08 +0100

    add styling to detail page

--- app/views.py
+++ app/views.py
@@ -7,3 +7,7 @@ from .models import Product
 def index(request):
     products = Product.objects.all()
     return render(request, 'app/index.html', {"products":products})
+
+def detail(request, id):
+    product = Product.objects.get(id=id)
+    return render(request, 'app/detail.html', {"product":product})
